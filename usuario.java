2e71d0fbbc829c400bb060ea4c58296b7e659e6c
PilarBanchero <pilarbanchero@gmail.com>
2020-04-04 17:53:34 -0300

    Commit A creacion de atributos

---
+++ usuario.java
@@ -0,0 +1,15 @@
+public class usuario{
+    
+    private String nombre;
+    private String cedula;
+    private String contraseña;
+    private int edad;
+    private int cantidadDeHijos;
+    private int saldo;
+           
+           
+   
+
+    
+   
+}
