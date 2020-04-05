07e44a5dd5dc805d247fadf3d4c281f01b65ca36
PilarBanchero <pilarbanchero@gmail.com>
2020-04-04 21:32:16 -0300

    Commit I

---
+++ FuncionesUsuario.java
@@ -0,0 +1,26 @@
+
+public class FuncionesUsuario {
+
+    public int calculoImpuestos(int edad, int cantidadDeHijos) {
+        int impuesto;
+
+        if (edad < 18) {
+            impuesto = 0;
+        } else {
+            if (edad <= 30 || edad > 65) {
+                impuesto = 2000;
+            } else {
+                impuesto = 7000;
+            }
+            if (cantidadDeHijos >= 1) {
+                int porcentaje = ((3 * impuesto) / 100 )* cantidadDeHijos;
+                if (porcentaje * 2 < impuesto) {
+                    impuesto = impuesto - porcentaje;
+                } else {
+                    impuesto = impuesto / 2;
+                }
+            }
+        }
+        return impuesto;
+    }
+}
